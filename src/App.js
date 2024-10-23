e9f84f697e62268737367414caed88b1f7739bf4
Gopi Kishore <gopikishore777@gmail.com>
2024-10-23 18:01:03 +0100

    Added aboutus page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import Header from "./components/Header";
 import Body from './components/Body';
- import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import About from './pages/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-       <Header/> 
-      <Body/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/aboutUs" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+   
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../css/Footer.css';
+function Footer() {
+  return (
+    <div className="footer">
+        <div className='footer_center'>
+            <ul className='list_ul'>
+                <li><a href="/aboutUs">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Join Us</a></li>
+                <li><a href="#">Feedback</a></li>
+                <li><a href="#">Donation</a></li>
+            </ul>
+        
+        </div>
+    </div>
+
+  )
+}
+
+export default Footer
